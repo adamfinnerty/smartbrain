9c66af82a68d9695bcbfbbfe63a5585f943e2d4c
adamfinnerty <adamfinnerty@gmail.com>
2019-08-19 22:37:40 +0100

    Adding the States

--- src/App.js
+++ src/App.js
@@ -9,6 +9,21 @@ import './App.css';
 
 
 class App extends Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit =() => {
+    console.log('click');
+  }
   render() {
   return (
     <div className="App">
@@ -16,7 +31,7 @@ class App extends Component {
     <Navigation />
     <Logo />
     <Rank />
-    <ImageLinkForm />
+    <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
     {/*<FaceRecognition />*/}
     </div>
   );
